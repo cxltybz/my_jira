5b927b2a64d490115dfd3a1b30b95d1a2a1c6ee9
cxltybz <1324071204@qq.com>
2024-03-04 13:43:36 +0800

    feat: ts强类型

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { User } from "./search-panel";
+interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: string;
+  organization: string;
+}
 
-export const List = ({ list, users }) => {
+interface ListProps {
+  list: Project[];
+  users: User[];
+}
+
+export const List = ({ list, users }: ListProps) => {
   return (
     <table>
       <thead>
@@ -14,8 +27,8 @@ export const List = ({ list, users }) => {
           <tr key={project.id}>
             <td>{project.name}</td>
             <td>
-              {users.find((user) => Number(user.id) === project.personId)
-                ?.name || "未知"}
+              {users.find((user) => user.id === project.personId)?.name ||
+                "未知"}
             </td>
           </tr>
         ))}
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-const isFalsy = (value) => (value === 0 ? false : !value);
+const isFalsy = (value: any) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不对的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // object.assign({},object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay);
