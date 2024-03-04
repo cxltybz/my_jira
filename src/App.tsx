a86cf1d093d2fc4d194c885b486299915182ab61
cxltybz <1324071204@qq.com>
2024-03-04 14:10:08 +0800

    feat: 用react表单、ts的类型继承和鸭子类型实现就登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
-import { TsReactTest } from "./try-use-array";
+// import { TsReactTest } from "./try-use-array";
+import { Login } from "screens/login";
 
 function App() {
   return (
     <div className="App">
       {/* <ProjectListScreen /> */}
-      <TsReactTest />
+      {/* <TsReactTest /> */}
+      <Login />
     </div>
   );
 }
